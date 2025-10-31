bd6a43ec19557b068336d8073296d48dc8bac6ea
Moonwalker <moonwalker1092@gmail.com>
2025-10-31 17:53:59 +0530

    Restore credit and subscription information

--- info.py
+++ info.py
@@ -90,7 +90,7 @@ PUBLIC_FILE_CHANNEL = environ.get('PUBLIC_FILE_CHANNEL', '') # Public Channel Us
 # Links
 GRP_LNK = environ.get('GRP_LNK', 'https//t.me/play_community_group')
 CHNL_LNK = environ.get('CHNL_LNK', 'https://t.me/play_tamil_dubbed_series')
-SUPPORT_CHAT = environ.get('SUPPORT_CHAT', 'play_community_group') # Support Chat Link Without https:// or @
+SUPPORT_CHAT = environ.get('SUPPORT_CHAT', 'https//t.me/play_community_group') # Support Chat Link Without https:// or @
 OWNER_LNK = environ.get('OWNER_LNK', 'https//t.me/Here_remo')
 
 # True Or False
@@ -196,6 +196,7 @@ else:
 # Don't Remove Credit @VJ_Bots
 # Subscribe YouTube Channel For Amazing Bot @Tech_VJ
 # Ask Doubt on telegram @KingVJ01
+
 
 
 
